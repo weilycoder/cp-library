3b3faf765fa148dd7d11d7ac000f188ca9686e63
weilycoder <weily2009@outlook.com>
2025-10-29 23:15:09 +0800

    feat: Add aplusb test for basic addition functionality and remove many_aplusb_debug test

---
+++ test/aplusb.test.cpp
@@ -0,0 +1,13 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../weilycoder/fast-io.hpp"
+using namespace weilycoder;
+
+static FastIOStd<true> io;
+
+int main() {
+  uint64_t a = io.read_u64();
+  uint64_t b = io.read_u64();
+  io.write_u64_line(a + b);
+  return 0;
+}
