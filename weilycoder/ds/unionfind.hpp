638ba1ed58ba0f548cf6477b8eaca0181dedd885
weilycoder <weily2009@outlook.com>
2025-10-30 19:59:26 +0800

    docs: Enhance documentation for UnionFind class with detailed method descriptions

--- weilycoder/ds/unionfind.hpp
+++ weilycoder/ds/unionfind.hpp
@@ -6,26 +6,61 @@
 #include <vector>
 
 namespace weilycoder {
+/**
+ * @brief Union-Find (Disjoint Set Union) Data Structure
+ * @tparam ptr_t Type for representing indices (default: size_t)
+ */
 template <typename ptr_t = size_t> struct UnionFind {
-  std::vector<ptr_t> parent, size;
+private:
+  std::vector<ptr_t> parent, setSize;
 
-  UnionFind(ptr_t n) : parent(n), size(n, 1) {
+public:
+  /**
+   * @brief Constructs a UnionFind with n elements, each in its own set
+   * @param n Number of elements
+   */
+  UnionFind(ptr_t n) : parent(n), setSize(n, 1) {
     std::iota(parent.begin(), parent.end(), 0);
   }
 
+  /**
+   * @brief Returns the number of elements
+   * @return Number of elements
+   */
+  size_t size() const { return parent.size(); }
+
+  /**
+   * @brief Returns the size of the set containing x
+   * @param x Element to query
+   * @return Size of the set containing x
+   */
+  size_t get_size(ptr_t x) { return setSize[getf(x)]; }
+
+  /**
+   * @brief Finds the representative of the set containing x
+   * @param x Element to find
+   * @return Representative of the set containing x
+   */
   ptr_t getf(ptr_t x) {
     while (x != parent[x])
       x = parent[x] = parent[parent[x]];
     return x;
   }
 
+  /**
+   * @brief Unites the sets containing x and y
+   * @param x First element
+   * @param y Second element
+   * @return True if the sets were separate and have been united, false if they
+   *         were already in the same set
+   */
   bool unite(ptr_t x, ptr_t y) {
     x = getf(x), y = getf(y);
     if (x == y)
       return false;
-    if (size[x] < size[y])
+    if (setSize[x] < setSize[y])
       std::swap(x, y);
-    parent[y] = x, size[x] += size[y];
+    parent[y] = x, setSize[x] += setSize[y];
     return true;
   }
 };
