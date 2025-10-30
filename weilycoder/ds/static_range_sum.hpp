57475f7f60ddb7171e247b68912fa579b58c7dbc
weilycoder <weily2009@outlook.com>
2025-10-30 19:51:16 +0800

    docs: Improve documentation for StaticRangeSum with detailed constructor and method descriptions

--- weilycoder/ds/static_range_sum.hpp
+++ weilycoder/ds/static_range_sum.hpp
@@ -7,6 +7,11 @@
 #include <vector>
 
 namespace weilycoder {
+/**
+ * @brief Static Range Sum using Prefix Sums
+ * @tparam Group A group defining the operation and identity element,
+ *                must be associative and invertible (i.e. Group).
+ */
 template <typename Group> struct StaticRangeSum {
   using value_type = typename Group::value_type;
   using T = value_type;
@@ -15,12 +20,23 @@ private:
   std::vector<T> prefix_sum;
 
 public:
+  /**
+   * @brief Constructs a StaticRangeSum for n elements initialized to the
+   *        identity element
+   * @param n Number of elements
+   */
   explicit StaticRangeSum(const std::vector<T> &data)
       : prefix_sum(data.size() + 1, Group::identity()) {
     for (size_t i = 1; i <= data.size(); ++i)
       prefix_sum[i] = Group::operation(prefix_sum[i - 1], data[i - 1]);
   }
 
+  /**
+   * @brief Constructs a StaticRangeSum from an initial range
+   * @tparam InputIt Input iterator type
+   * @param first Beginning of the range
+   * @param last End of the range
+   */
   template <typename InputIt>
   StaticRangeSum(InputIt first, InputIt last)
       : prefix_sum(std::distance(first, last) + 1, Group::identity()) {
@@ -29,8 +45,19 @@ public:
       prefix_sum[i] = Group::operation(prefix_sum[i - 1], *it);
   }
 
+  /**
+   * @brief Returns the number of elements
+   * @return Number of elements
+   */
   size_t size() const { return prefix_sum.size() - 1; }
 
+  /**
+   * @brief Queries the sum in the range [l, r)
+   * @param l Left index (inclusive)
+   * @param r Right index (exclusive)
+   * @return Sum in the range [l, r)
+   * @throws std::out_of_range if the range is invalid
+   */
   T query(size_t l, size_t r) const {
     if (l > r || r > size())
       throw std::out_of_range("Invalid range for StaticRangeSum query");
